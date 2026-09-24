5d825f4cd436080f9204ea4474549917980894aa
nayakantiparimala-web <nayakantiparimala@gmail.com>
2026-09-24 12:33:51 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,24 @@
+function calculate(operator) {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let result;
+
+    if (operator === "+") {
+        result = num1 + num2;
+    } 
+    else if (operator === "-") {
+        result = num1 - num2;
+    } 
+    else if (operator === "*") {
+        result = num1 * num2;
+    } 
+    else if (operator === "/") {
+        if (num2 === 0) {
+            document.getElementById("result").innerText = "Cannot divide by zero";
+            return;
+        }
+        result = num1 / num2;
+    }
+
+    document.getElementById("result").innerText = result;
+}
